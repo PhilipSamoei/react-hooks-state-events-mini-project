638c228003cc2023e4c98071fb2c456e5d362ec4
Philip Samoei <samphilkerich04@gmail.com>
2023-04-25 10:59:09 +0300

    complete assignment

--- src/components/App.js
+++ src/components/App.js
@@ -8,12 +8,13 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const handleDeleteButton = TASKS.filter((task)=>task.id !== task)
   return (
     <div className="App">
       <h2>My tasks</h2>
       <CategoryFilter />
       <NewTaskForm />
-      <TaskList />
+      <TaskList tasks={TASKS} onDelete={handleDeleteButton}/>
     </div>
   );
 }
